db3f8075c4c0e3074eb608e075a1847b13cf54c3
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-05-17 21:47:33 +0200

    add semigroup snippets

---
+++ src/main/scala/snippets/semigroup/SemigroupExample.scala
@@ -0,0 +1,10 @@
+package snippets.semigroup
+
+import cats.Semigroup
+
+object SemigroupExample extends App {
+  implicit val multiplicationSemigroup: Semigroup[Int] = (x: Int, y: Int) =>
+    x * y
+
+  print(Semigroup[Int].combine(3, 3))
+}
