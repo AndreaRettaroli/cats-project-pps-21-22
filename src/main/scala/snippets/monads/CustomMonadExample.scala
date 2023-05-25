65b33f0b5f8d33060bfcaddc6bd056803cc76aad
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-05-25 23:20:49 +0200

    fix custom monad example

--- src/main/scala/snippets/monads/CustomMonadExample.scala
+++ src/main/scala/snippets/monads/CustomMonadExample.scala
@@ -3,8 +3,9 @@ package snippets.monads
 import cats.implicits._
 
 object CustomMonadExample extends App {
-  for {
+  val res = for {
     a <- CustomMonad(1)
     b <- CustomMonad(2).flatMap(x => CustomMonad(x + 1))
-  } yield print(a + b)
+  } yield a + b
+  println(res)
 }
